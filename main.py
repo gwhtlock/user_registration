5f676c64160bcfb2273cf31021697627ddc0a32c
Aaron Whitlock <whitlock.aaron@yahoo.com>
2019-04-18 13:20:20 -0500

    add checks for password and password verify

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-def check_username(login_name):
+def check_input(login_name):
 
     name = login_name
 
     user_error = ""
    
     if name == "":
-        user_error = "Please enter a username"
+        user_error = "Please enter a value"
     elif len(name) < 3:
-        user_error = "Username is too short. Must be more than 3 characters"
+        user_error = "Input is too short. Must be more than 3 characters"
     elif len(name) > 20:
-        user_error = "Username is too long. Must be less than 20 characters"
+        user_error = "Input is too long. Must be less than 20 characters"
+    elif " " in name:
+        user_error = "Input can't contain a space!!"
     
 
 
@@ -34,16 +36,25 @@ def user_name():
     password1 = request.form['password1']
     password2 = request.form['password2']
 
-    user_errors = check_username(username)
+    user_errors = check_input(username)
+    password1_error = check_input(password1)
+    password2_error = check_input(password2)
+
+    match_error =""
+
+    if password1 != password2:
+
+        match_error = "passwords do not match"
 
 
 
-    if user_errors != "":
-        return render_template('edit.html', error = user_errors, username_error = user_errors)
+    if user_errors != "" or password1_error != "" or password2_error != "" or match_error != "":
+        return render_template('edit.html', pass1_error = password1_error, pass2_error = password2_error, matching_error = match_error,
+         username_error = user_errors, username = username)
 
     
 
-    return render_template("confirmation.html",user = username, pw1 = password1, pw2 = password2 )
+    return render_template("confirmation.html",user = username )
 
 
 @app.route("/")
