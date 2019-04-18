40112f08028cf11f3d1c5df25c8bb67382b4a709
Aaron Whitlock <whitlock.aaron@yahoo.com>
2019-04-17 23:50:31 -0500

    added check to ensure all fields have a value

--- main.py
+++ main.py
@@ -5,7 +5,29 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+def check_input(login,pw1,pw2):
 
+    name = login
+    pass1 = pw1
+    pass2 = pw2
+
+    noname = ""
+    nopw1 = ""
+    nopw2 = ""
+
+    if name == "" or pass1 == "" or pass2 == "":
+
+        if name == "":
+            noname = "You didn't enter a Username!  "
+
+        if pass1 == "":
+            nopw1 = "You didn't enter a password!  "
+
+        if pass2 == "":
+            nopw2 = "You didn't re-enter a password!  "
+
+
+        return ("Please enter a value in each field. "+ "  " + noname + "  " + nopw1 +"   "+ nopw2+"  ")
 
 
 
@@ -18,18 +40,28 @@ def user_name():
     password1 = request.form['password1']
     password2 = request.form['password2']
 
-    if password1 != password2:
-        error = "passwords do not match"
-        return redirect("/?error"+error)
+    username_error = check_input(username, password1, password2)
 
-    return render_template("confirmation.html",user = username, pw1 = password1, pw2 = password2 )
+    if username_error != "":
+        return render_template('edit.html', error = username_error)
+
+    match_error = ""
+
+    if password1 != password2:
+        match_error = "passwords do not match"
+        return render_template('edit.html', error = match_error)
+        
+
+    return render_template("confirmation.html",user = username, pw1 = password1, pw2 = password2, error = match_error )
 
 
 @app.route("/")
 def index():
-    encoded_error = request.args.get("error")
     
-    return render_template('edit.html',error = encoded_error and cgi.escape(encoded_error, quote=True))
+    
+    return render_template('edit.html')
+
+
 
 
 
